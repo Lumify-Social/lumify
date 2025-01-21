1fd283109cf717dfc7bb4449343ef759d4e4bcd3
Max <max.pcgt@gmail.com>
2025-01-21 16:50:00 +0100

    Ajout des pages, README et LICENSE et initialisation de TailwindCSS - v.1.0.1

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'logo' => 'img/logo.png',
+        ]);
+    }
+
+    #[Route('/404', name: 'app_error')]
+    public function error(): Response
+    {
+        return $this->render('404.html.twig', [
+            'controller_name' => 'HomeController',
+            'logo' => 'img/logo.png',
+        ]);
+    }
+}
